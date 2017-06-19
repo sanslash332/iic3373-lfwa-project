986ed6d89997fa0a57129d6bee0637222051b5d9
jabozzo <jabozzo@uc.cl>
2017-06-19 12:57:43 -0400

    Dall

--- select_and_export_feats.m
+++ select_and_export_feats.m
@@ -5,12 +5,14 @@ l_eye_points = 21:26;
 nose_points  = 1:9;
 mouth_points = 32:51;
 
-eyes_size = [15 20];
-nose_size = [40 15];
+r_eye_size = [15 20];
+l_eye_size = [15 20];
+nose_size  = [40 15];
 mouth_size = [20 23];
+
 id = affine2d(eye(3));
 
-cropped_parts = cell(length(face_points.detections), 1);
+all_data = cell(length(face_points.detections), 1);
 
 fit_factor = 0.3;
 
@@ -29,17 +31,16 @@ for ii = 1:length(face_points.detections)
   
   fprintf("Processing %d/%d : %s\n", ii, length(face_points.detections), char(filename));
   
+  stru.filename = filename;
+  
   if(length(points) ~= 68)
     fprintf("  Could not process, skipping...\n");
-    stru.filename = filename;
     stru.success = 0;
-    stru.r_eye = zeros(eyes_size);
-    stru.l_eye = zeros(eyes_size);
-    stru.nose = zeros(nose_size);
-    stru.mouth = zeros(mouth_size);
   else  
     img = imread(filename);
   
+    stru.filename = filename;
+    
     % [minx miny maxx maxy]
     r_eye_positions = newpoints(r_eye_points,:);
     l_eye_positions = newpoints(l_eye_points,:);
@@ -48,13 +49,13 @@ for ii = 1:length(face_points.detections)
   
     r_eye_ranges = [min(r_eye_positions(:,1)) min(r_eye_positions(:,2)) max(r_eye_positions(:,3)) max(r_eye_positions(:,4))];
     l_eye_ranges = [min(l_eye_positions(:,1)) min(l_eye_positions(:,2)) max(l_eye_positions(:,3)) max(l_eye_positions(:,4))];
-    nose_ranges = [min(nose_positions(:,1)) min(nose_positions(:,2)) max(nose_positions(:,3)) max(nose_positions(:,4))];
+    nose_ranges =  [min(nose_positions(:,1))  min(nose_positions(:,2))  max(nose_positions(:,3))  max(nose_positions(:,4))];
     mouth_ranges = [min(mouth_positions(:,1)) min(mouth_positions(:,2)) max(mouth_positions(:,3)) max(mouth_positions(:,4))];
   
-    r_eye_crop = imref2d(eyes_size, [r_eye_ranges(1) r_eye_ranges(3)], [r_eye_ranges(2) r_eye_ranges(4)]);
+    r_eye_crop = imref2d(r_eye_size, [r_eye_ranges(1) r_eye_ranges(3)], [r_eye_ranges(2) r_eye_ranges(4)]);
     r_eye = imwarp(img, id, 'cubic', 'OutputView', r_eye_crop);
   
-    l_eye_crop = imref2d(eyes_size, [l_eye_ranges(1) l_eye_ranges(3)], [l_eye_ranges(2) l_eye_ranges(4)]);
+    l_eye_crop = imref2d(l_eye_size, [l_eye_ranges(1) l_eye_ranges(3)], [l_eye_ranges(2) l_eye_ranges(4)]);
     l_eye = imwarp(img, id, 'cubic', 'OutputView', l_eye_crop);
   
     nose_crop = imref2d(nose_size, [nose_ranges(1) nose_ranges(3)], [nose_ranges(2) nose_ranges(4)]);
@@ -63,16 +64,13 @@ for ii = 1:length(face_points.detections)
     mouth_crop = imref2d(mouth_size, [mouth_ranges(1) mouth_ranges(3)], [mouth_ranges(2) mouth_ranges(4)]);
     mouth = imwarp(img, id, 'cubic', 'OutputView', mouth_crop);
   
-    stru.filename = filename;
     stru.success = 1;
-    stru.r_eye = r_eye;
-    stru.l_eye = l_eye;
-    stru.nose = nose;
-    stru.mouth = mouth;
+    stru.data = {r_eye, l_eye, nose, mouth};
     
-    cropped_parts{ii} = stru;
   end
+  
+  all_data{ii} = stru;
   
 end
 
-save('cropped_parts.mat', 'cropped_parts');
+save('all_data.mat', 'all_data');
