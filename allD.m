81429c9f49ae615f98eed33ef21b7a3afbf1453e
jabozzo <jabozzo@uc.cl>
2017-06-19 22:13:13 -0400

    Ultima vez que uso sparse.

--- allD.m
+++ allD.m
@@ -1,18 +1,107 @@
-%% Creaci�n de Diccionario 
-% Solo se crea con las del testing.
+face_points = load('./raw_faces_points_2.mat');
 
-load('all_data.mat');
+offsets = [ 0.02,  0.02;...
+            0.02,  0, ;...
+            0.02, -0.02;...
+            0   ,  0.02;...
+            0   ,  0, ;...
+            0   , -0.02;...
+           -0.02,  0.02;...
+           -0.02,  0, ;...
+           -0.02, -0.02];
 
-offsets = [ 0.1,  0.1;...
-            0.1,  0, ;...
-            0.1, -0.1;...
-            0  ,  0.1;...
-            0  ,  0, ;...
-            0  , -0.1;...
-           -0.1,  0.1;...
-           -0.1,  0, ;...
-           -0.1, -0.1];
+n_offsets = size(offsets, 1);
+       
+r_eye_points = 10:15;
+l_eye_points = 21:26;
+nose_points  = 1:9;
+mouth_points = 32:51;
 
-[D, person, photo, offset, patch] = parts_to_dict(all_data, offsets);
+all_points = {r_eye_points, l_eye_points, nose_points, mouth_points};
 
-save('Dall.mat','D');
+r_eye_size = [15 20];
+l_eye_size = [15 20];
+nose_size  = [30 10];
+mouth_size = [15 20];
+
+all_sizes = [r_eye_size; l_eye_size; nose_size; mouth_size];
+
+id = affine2d(eye(3));
+
+all_data = cell(length(face_points.detections)*n_offsets, 1);
+
+fit_factor = 0.3;
+
+person = uint32(zeros(length(all_data), 1));
+offset = uint32(zeros(size(person, 1), 1));
+photo  = uint32(zeros(size(person, 1), 1));
+patch  = uint32(zeros(size(person, 1), 1));
+
+for ii = 1:length(face_points.detections)
+  data = face_points.detections{ii};
+  filename = data{1};
+  points = data{2}.xy;
+
+  newpoints = zeros(size(points));
+  
+  newpoints(:,1) = (1-fit_factor)*points(:,1) + 0.5*fit_factor*(points(:,1) + points(:,3));
+  newpoints(:,3) = (1-fit_factor)*points(:,3) + 0.5*fit_factor*(points(:,1) + points(:,3));
+
+  newpoints(:,2) = (1-fit_factor)*points(:,2) + 0.5*fit_factor*(points(:,2) + points(:,4));
+  newpoints(:,4) = (1-fit_factor)*points(:,4) + 0.5*fit_factor*(points(:,2) + points(:,4));
+  
+  fprintf("Processing %d/%d : %s\n", ii, length(face_points.detections), char(filename));
+  
+  stru = struct();
+  stru.filename = filename;
+  
+  if(length(points) ~= 68)
+    fprintf("  Could not process, skipping...\n");
+    stru.success = 0;
+    
+    for off_i = 1:n_offsets
+      all_data{(ii - 1)*n_offsets + off_i} = stru;  
+    end
+  else  
+    stru.success = 1;
+    img = imread(filename);
+  
+    stru.filename = filename;
+    
+    % [minx miny maxx maxy]
+    ranges = zeros(length(all_points), 4);
+    for c_point_i = 1:length(all_points)
+      positions = newpoints(all_points{c_point_i},:);
+      ranges(c_point_i, :) = [min(positions(:,1)) min(positions(:,2)) max(positions(:,3)) max(positions(:,4))];
+    end
+    
+    for off_i = 1:size(offsets, 1)
+      off = offsets(off_i, :);
+      
+      patches = cell(length(all_points), 1);
+      for patch_i = 1:length(patches)
+        crop = off.*(ranges(patch_i, 3:4) - ranges(patch_i, 1:2));
+        crop = ranges(patch_i,:) + [crop crop];
+        crop_transform = imref2d(all_sizes(patch_i, :), [crop(1) crop(3)], [crop(2) crop(4)]);
+        patches{patch_i} = imwarp(img, id, 'cubic', 'OutputView', crop_transform);
+        stru.data = patches;
+        index = (ii - 1)*n_offsets + off_i;
+        all_data{index} = stru;
+        offset(index) = off_i;
+        patch(index)  = patch_i;
+        person(index) = uint32(str2num(filename(6:9)));
+        photo(index)  = uint32(str2num(filename(11:14)));
+      end
+    end
+  end
+end
+
+[Dall, used_index] = parts_to_dict(all_data);
+
+patch  = patch(used_index);
+photo  = photo(used_index);
+person = person(used_index);
+offset = offset(used_index);
+
+save('Dall.mat','Dall', 'person', 'photo', 'patch', 'offset');
+clear all;
---
+++ program.m
@@ -0,0 +1,27 @@
+fprintf("Loading data...\n")
+load('./Dall.mat');
+
+param.L          = 50;           % not more than L non-zeros coefficients
+param.eps        = 0;           % optional, threshold on the squared l2-norm of the residual, 0 by default
+param.numThreads = -1;          % number of processors/cores to use; the default choice is -1 (it selects all the available CPUs/cores)
+
+fprintf("Selecting train/test set...\n")
+[train_set, test_set] = proyect_holdout(person, photo, patch, offset, 0, 10);
+fprintf("Creating Y...\n")
+[Y_holdout, person_holdout] = YfromD(Dall, test_set, person, photo);
+
+fprintf("Creating sparse representation...\n")
+D_holdout = Dall(:, train_set);
+fprintf("Patience...\n")
+
+X_holdout = full(mexOMP(Y_holdout,D_holdout,param));
+X_holdout = X_holdout';
+
+%%
+fprintf("Predicting person...\n")
+prediction_holdout = predict_from_X(D_holdout, X_holdout, Y_holdout, person_holdout);
+
+fprintf("Computing confusion matrix...\n")
+Evaluate(person_holdout', prediction_holdout')
+%plotconfusion(person_holdout, prediction_holdout);
+
