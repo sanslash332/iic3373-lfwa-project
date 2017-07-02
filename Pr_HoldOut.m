1fdc489230a608fa0b2812789c2068ffe4ae1892
FranciscoFS <fdfernandez@uc.cl>
2017-07-02 17:09:21 -0400

    Multi Test
    Ya seba, ahi evalua para mas de un classificador

--- Pr_HoldOut.m
+++ Pr_HoldOut.m
@@ -10,17 +10,34 @@ function [Accuracy] = Pr_HoldOut(features,foto,persona,k)
 %     Carac_clean = Carac_norm(indice_training,Indice_clean);
     [PCA_Matrix,Score,~] = pca(Carac_norm(indice_training,:),'NumComponents',k);
     
-    disp('Training model..... ');
-    Model = fitcdiscr(Score,persona(indice_training),'DiscrimType','pseudoLinear');
+    disp('Training model LDA..... ');
+    Model{1}.m = fitcdiscr(Score,persona(indice_training),'DiscrimType','pseudoLinear');
+    Model{1}.name = 'LDA';
+    disp('Training model QDA..... ');
+    Model{2}.m = fitcdiscr(Score,persona(indice_training),'DiscrimType','diagquadratic');
+    Model{2}.name = 'qda';
+    disp('Training model KNN10..... ');
+    Model{3}.m = fitcknn(Score,persona(indice_training),'NumNeighbors',10);
+    Model{3}.name = 'knn10';
+    disp('Training model KNN5..... ');
+    Model{4}.m = fitcknn(Score,persona(indice_training),'NumNeighbors',5);
+    Model{4}.name = 'knn5';
     
+%     tlda = templateDiscriminant('DiscrimType','pseudoLinear');
+%     Model = fitcecoc(Score,persona(indice_training),'Learners',tlda);
+
     Caracteristicas_test = Carac_norm(indice_test,:);
     X_pca_test = (Caracteristicas_test-repmat(mean(Caracteristicas_test),size(Caracteristicas_test,1),1))*PCA_Matrix;
     
-    disp('Predicting and evaluating ');
+  
+    Accuracy = {};
     
-    Y_predict = predict(Model, X_pca_test);
-    
-    Accuracy = Evaluate(persona(indice_test),Y_predict);
+    for k=1:length(Model)
+        fprintf('Predicting and Evaluating %s \n',Model{k}.name);
+        Y_predict = predict(Model{k}.m, X_pca_test);
+        Accuracy{k}.acc = Evaluate(persona(indice_test),Y_predict);
+        Accuracy{k}.model = Model{k}.name;
+    end
     
 end
     
